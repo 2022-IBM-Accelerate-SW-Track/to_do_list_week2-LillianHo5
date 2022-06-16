6d1089a41419d413353a80ddb87d9c1835f04e4d
LillianHo5 <holillian5@gmail.com>
2022-06-15 18:20:45 -0700

    Completed lab

--- src/pages/About.js
+++ src/pages/About.js
@@ -6,26 +6,31 @@ export default class About extends Component {
   render() {
     return (
       <div>
-        {/* <p>Design your About me page </p> */}
-        <div class="split left">
-          <div className="centered">
-            <img
-              className="profile_image"
-              src={profile_pic}
-              alt="Profile Pic"
-            ></img>
+        <div>
+          <div class="split left">
+            <div className="centered">
+              <img
+                className="profile_image"
+                src={profile_pic}
+                alt="Profile Pic"
+              ></img>
+            </div>
           </div>
-        </div>
-        <div className="split right">
-          <div className="centered">
-            <div className="name_title">Your Name</div>
-            <div className="brief_description">
-              Tell us about yourself in a few sentences. Tell us your interests
-              and say a fun fact about yourself.
+          <div className="split right">
+            <div className="centered">
+              <div className="name_title">Lillian Ho</div>
+              <div className="brief_description">
+                <p>
+                  Hello! My name is Lillian Ho, and I am a rising second year at UC San Diego studying computer science. In 
+                  my free time, I enjoy going on hikes and spending time with my loved ones. I also enjoy playing video
+                  games and occasionally knitting.
+                </p>
+              </div>
             </div>
           </div>
         </div>
+
       </div>
-    );
+    )
   }
 }
